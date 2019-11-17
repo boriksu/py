0914a7e7202b94bbb965ff534a7f1dc2e2f0532d
Debra Holiday <dholiday@mi-h5.21-school.ru>
2019-11-17 18:01:18 +0300

    hello read file, draw lines and rooms, and button quit

--- test.py
+++ test.py
@@ -1,13 +1,55 @@
 import fileinput
+from random import *
+from tkinter import *
+from time import *
 
-dictionary = {}
-for line in fileinput.input():
-    line = line.rstrip()
-    if line.find(' ') != -1:
-        new = line.split(maxsplit=1)
-        dictionary[new[0]] = new[1]
-        print(new)
-    elif line.find('-') != -1:
-        new = line.split('-')
-        print(new)
-print(dictionary.items())
+
+
+def draw_rooms(rooms):
+    colors = choice(['pink', 'lime'])
+    for key in rooms:
+        x = rooms[key][0]
+        y = rooms[key][1]
+        canvas.create_oval(x, y, x+20, y+20, fill=colors)
+        canvas.create_text(x + 10, y + 10, anchor=W, font="Purisa", text=key)
+        root.update()
+
+
+def draw_links(rooms, links):
+    for el in links:
+        f = rooms[el[0]]
+        s = rooms[el[1]]
+        canvas.create_line(f[0] + 10, f[1] + 10, s[0] + 10, s[1] + 10)
+        root.update()
+
+def read_map(rooms, links):
+    for line in fileinput.input():
+        line = line.rstrip()
+        print(line)
+        if line.find(' ') != -1:
+            new = line.split(maxsplit=1)
+            coordi = new[1].split(' ')
+            coordi[0] = (int(coordi[0])  + 10) * 15
+            coordi[1] = (int(coordi[1])  + 10) * 15
+            rooms[new[0]] = coordi
+        elif line.find('-') != -1:
+            new = line.split('-')
+            links.append(new)
+
+def Quit(ev):
+    global root
+    root.destroy()
+
+rooms = {}
+links = []
+size = 600
+root = Tk()
+canvas = Canvas(root, width=size, height=size)
+canvas.pack()
+read_map(rooms, links)
+draw_rooms(rooms)
+draw_links(rooms, links)
+#sleep(10)
+#input('Press ENTER to exit')
+Button(root, text="Quit", command=root.destroy).pack()
+root.mainloop()
